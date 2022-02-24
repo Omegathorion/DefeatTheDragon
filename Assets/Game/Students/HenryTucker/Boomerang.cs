df0badb2f614971b40c76e40fa1464a1b076a3e1
HenryTucker <prodigy1202@gmail.com>
2022-02-23 20:27:49 -0500

    Added Boomerang Relic
    There is still an error coming from EndTurn

---
+++ Assets/Game/Students/HenryTucker/Boomerang.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ScriptableObjectArchitecture;
+
+
+public class Boomerang : MonoBehaviour
+{
+    [SerializeField] DeckManager deckManager;
+    public int amountOfCardsDrawnFromDiscard;
+    public GameObjectGameEvent drawCardEvent;
+
+    public void WhenCardsAreDrawnAtTurnStart()
+    {
+        deckManager = transform.root.GetComponentInChildren<DeckManager>();
+        Card[] discardedCards = deckManager.discard.GetComponentsInChildren<Card>();
+        if (discardedCards.Length >= 1)
+        {
+            GameObject cardDrawn = discardedCards[Random.Range(0, discardedCards.Length)].gameObject;
+            drawCardEvent.Raise(cardDrawn);
+        }
+        //GameObject cardFromDiscard = discardPile.GetChild(Random.Range(0, discardPile.GetChild.length).gameObject);
+    }
+}
