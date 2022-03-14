cf78122d1eca8defcaf4480bd9a078208b669997
leeleemcn <61673061+leeleemcn@users.noreply.github.com>
2022-03-14 15:16:19 -0400

    PoisonTick Finished

--- Assets/Game/Students/LeeLeeMcNeil/PoisonTick.cs
+++ Assets/Game/Students/LeeLeeMcNeil/PoisonTick.cs
@@ -3,94 +3,116 @@ using System.Collections.Generic;
 using UnityEngine;
 using ScriptableObjectArchitecture;
 
-//public class PoisonTick : Card, ITargetSingleEnemy // targeter for all enemies? 
-//{
-//    public GameObject processorPrefab;
+public class PoisonTick : Card, ITargetAllEnemies // targeter for all enemies? 
+{
+    public GameObject processorPrefab;
     
-//    public GameObjectGameEvent onCardPlayed;
-//    private GameObject currentProcessor;
-//    public CallForInterjectionsGameEvent interjectionEvent;
-
-//    public GameObject target;
-//    public GameObject targeterPrefab;
-//    private GameObject currentTargeter;
+    public GameObjectGameEvent onCardPlayed;
+    
+    public CallForInterjectionsGameEvent interjectionEvent;
 
     
+    public GameObject targeterPrefab;
+    private GameObject currentTargeter;
 
+    public GameObject poisonPrefab;
+
+    public List<GameObject> targets = new List<GameObject>();
     
-//    public int PoisonAmount;
+    public int PoisonAmount;
+    private List<GameObject> handlist = new List<GameObject>();
+    private GameObject cardInUse;
+    public int DiscardAmt;
+    private int randomDiscardnum;
+    public override void OnMouseUp()
+    
+    {
+        if (targets.Count == 0)
+        {
+        }
+       else
+        {
+            GameObject manaProcessor = Instantiate(processorPrefab);
+            manaProcessor.GetComponent<InterjectionProcessor>().startingValue = manaCost;
+            interjectionEvent.Raise(new CallForInterjections(this.gameObject, this.gameObject, InteractionType.Mana, manaProcessor.GetComponent<InterjectionProcessor>()));
+            int processedManaCost = manaProcessor.GetComponent<InterjectionProcessor>().CalculateFinalValue();
+            if (playerMana >= processedManaCost)
+            {
 
-//    public override void OnMouseUp()
-//    {
-//        if (target == null)
-//        {
-//        }
-//        else
-//        {
-//            GameObject manaProcessor = Instantiate(processorPrefab);
-//            manaProcessor.GetComponent<InterjectionProcessor>().startingValue = manaCost;
-//            interjectionEvent.Raise(new CallForInterjections(this.gameObject, this.gameObject, InteractionType.Mana, manaProcessor.GetComponent<InterjectionProcessor>()));
-//            int processedManaCost = manaProcessor.GetComponent<InterjectionProcessor>().CalculateFinalValue();
-//            if (playerMana >= processedManaCost)
-//            {
+                playerMana.Value -= processedManaCost;
+               discardCardEvent.Raise(this.gameObject);
+              foreach (GameObject target in targets)
+                {
+                    GameObject instantiatedPoison = Instantiate(poisonPrefab);
 
-//                playerMana.Value -= processedManaCost;
-//                discardCardEvent.Raise(this.gameObject);
+                    GameObject poisonProcessor = Instantiate(processorPrefab);
+                    poisonProcessor.GetComponent<InterjectionProcessor>().startingValue = PoisonAmount;
+                    interjectionEvent.Raise(new CallForInterjections(this.gameObject, target, InteractionType.Status, poisonProcessor.GetComponent<InterjectionProcessor>()));
+                    instantiatedPoison.GetComponent<Status>().value = poisonProcessor.GetComponent<InterjectionProcessor>().CalculateFinalValue();
+                    Destroy(poisonProcessor);
+                    target.GetComponent<ITakeStatus>().TakeStatus(this.gameObject, instantiatedPoison);
+                }
+              for (int Index = 0; Index < DiscardAmt; Index ++)
+                {
+                    DiscardRandomCard();
+                }
+                
+                onCardPlayed.Raise(this.gameObject);
+            }
+            else
+            {
 
-//                //GameObject instantiatedPoison = Instantiate(); do we already have a poison status? I believe so? Should I call for that?
-//                GameObject poisonProcessor = Instantiate(processorPrefab);
-//                poisonProcessor.GetComponent<InterjectionProcessor>().startingValue = PoisonAmount;
-//                interjectionEvent.Raise(new CallForInterjections(this.gameObject, target, InteractionType.Status, poisonProcessor.GetComponent<InterjectionProcessor>()));
-//                instantiatedPoison.GetComponent<Status>().value = poisonProcessor.GetComponent<InterjectionProcessor>().CalculateFinalValue();
-//                Destroy(poisonProcessor);
-//                target.GetComponent<ITakeStatus>().TakeStatus(this.gameObject, instantiatedPoison);
-//                onCardPlayed.Raise(this.gameObject);
-//            }
-//            else
-//            {
+            }
+            Destroy(manaProcessor);
+        }
+        Deinitiate();
+    }
 
-//            }
-//            Destroy(manaProcessor);
-//        }
-//        Deinitiate();
-//    }
+    public void Deinitiate()
+    {
+        if (currentTargeter != null)
+        {
+            Destroy(currentTargeter);
+        }
+      
+        currentTargeter = null;
 
-//    public void Deinitiate()
-//    {
-//        if (currentTargeter != null)
-//        {
-//            Destroy(currentTargeter);
-//        }
-//        if (currentProcessor != null)
-//        {
-//            Destroy(currentProcessor);
-//        }
-//        currentTargeter = null;
-//        currentProcessor = null;
-//        target = null;
-//    }
+        targets.Clear();
+    }
 
-//    public override void OnMouseDown()
-//    {
-//        base.OnMouseDown();
-//        if (currentTargeter == null)
-//        {
-//            currentTargeter = GameObject.Instantiate(targeterPrefab);
-//            currentTargeter.GetComponent<Targeter>().requester = this.gameObject;
-//        }
-//    }
+    public override void OnMouseDown()
+    {
+        base.OnMouseDown();
+        if (currentTargeter == null)
+        {
+            currentTargeter = GameObject.Instantiate(targeterPrefab);
+            currentTargeter.GetComponent<Targeter>().requester = this.gameObject;
+        }
+    }
 
-//    //Also do we have a targeter for all enemies now? 
-//    public void ReceiveSingleEnemyTarget(GameObject receivedTarget)
-//    {
-//        target = receivedTarget;
-//        if (receivedTarget == null)
-//        {
-//            if (currentProcessor != null)
-//            {
-//                Destroy(currentProcessor);
-//            }
-//        }
+    //Also do we have a targeter for all enemies now? 
+    public void ReceiveAllEnemyTargets(List<GameObject> receivedEnemies)
+    {
+        targets = receivedEnemies;
+        
 
-//    }
-//}
+    }
+
+    public void DiscardRandomCard()
+    {
+        handlist.Clear();
+        foreach (Card cardInUse in this.transform.root.GetComponentInChildren<DeckManager>().hand.GetComponentsInChildren<Card>())
+        {
+            handlist.Add(cardInUse.gameObject);
+            
+        }
+        if (handlist.Count > 0)
+        {
+            randomDiscardnum = Random.Range(0, handlist.Count);
+            discardCardEvent.Raise(handlist[randomDiscardnum]);
+        }
+        
+
+
+}
+}
